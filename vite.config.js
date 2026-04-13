31f06b64047a5650ef098b0b09b31b341964195a
2327302232 <2327302232@qq.com>
2026-04-13 12:39:33 +0800

    整理&build

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,9 @@ import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
   plugins: [vue()],
-  base: "/", // 自定义域 + 根路径
+  base: "/", // 自��义域根路径
   build: {
-    outDir: ".",        // 构建产物输出到仓库根目录（给 Pages 用）
-    assetsDir: "assets",
-    emptyOutDir: false  // 不要清空根目录，避免删掉 CNAME / 源码
+    outDir: "dist",
+    emptyOutDir: true
   }
 });
